760e5b6ede9f51ca894016aa83b3952caa7cc498
staywill <staywillggez@gmail.com>
2025-02-03 22:52:56 +0000

    added First.py

---
+++ Steph/First.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# Author: Steph
+# Version: 1.0
+# Description: This is our first script
+
+name = "Steph"
+print ("my name is", name)
+print ("my name is " + name)
+
+import random
+print ("lucky number is", random.randint(1, 50))
+
+import math
+print ("cosine of 0.5 is", math.cos (0.5))
